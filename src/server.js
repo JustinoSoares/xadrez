33a062e80cc1a462f4fa11f88ff8d88c698fa59f
Justino Soares <justinocsoares123@gmail.com>
2024-12-19 13:01:14 +0100

    deploy

--- src/server.js
+++ src/server.js
@@ -1,30 +1,27 @@
-// src/index.js
-
 const express = require("express");
-const { sequelize } = require("../models/index.js"); // Certifique-se de importar corretamente
+const { sequelize } = require("../models/index.js");
 const usuarioRouter = require("./routes/user.router");
 const torneioRouter = require("./routes/torneio.router");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Rotas
 app.use("/", usuarioRouter);
 app.use("/", torneioRouter);
 
-// Testando a conexão antes de iniciar o servidor
+// Exportar o `app` para o Vercel
+module.exports = app;
+
+// Testar a conexão com o banco separadamente
 sequelize
   .authenticate()
   .then(() => {
     console.log("Conexão com o banco de dados estabelecida com sucesso.");
-
-    // Iniciar o servidor aqui após a conexão ser bem-sucedida.
-    return app;
-    // app.listen(port, () => {
-      // console.log(`Servidor rodando na porta ${port}.`);
-     //});
   })
   .catch((err) => {
     console.error("Não foi possível conectar ao banco de dados:", err);
